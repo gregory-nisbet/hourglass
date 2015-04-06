9ad2ca18f6ec04fe1ee5f78abf74ec6d60f54e9a
Gregory Nisbet <gregory.nisbet@gmail.com>
2015-04-05 20:56:12 -0700

    logs focused window to hard coded log

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var sys = require('sys');
 var exec = require('child_process').exec;
+var fs = require('fs');
 var child;
 
-exec("xprop -id `xdotool getwindowfocus`", function (error, stdout, stderr) {
-	console.log(stdout);
-});
+var wm_class = /WM_CLASS\(STRING\) = (.*)/;
 
-exec("pwd", function (error, stdout, stderr) {
-	console.log(stdout);
-});
+var xprop_info = function (xprop) {
+	var cls = (xprop.match(wm_class) || {})[1];
+	return cls;
+};
+
+var log_focused_window = function () {
+	exec("xprop -id `xdotool getwindowfocus`", function (error, stdout, stderr) {
+		var to_log = xprop_info(stdout);
+		if (to_log) {
+			fs.appendFile("/home/g/logs/log.txt", to_log+"\n", function (err) {
+				if (err) {
+					console.log(err);
+					throw new Error();
+				}
+			});
+		}
+	});
+};
+
+setInterval(log_focused_window, 99); 
+
